f992bbb3d26ef3126143f240acf5fc972a397a2c
Robert E. Novak <sailnfool@gmail.com>
2024-03-31 15:27:33 +0800

    Moved mount_disk to rentoy_lib.sh

--- ventoy-1.0.97/tool/UUIWorker.sh
+++ ventoy-1.0.97/tool/UUIWorker.sh
@@ -6,7 +6,21 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
- . ./tool/ventoy_lib.sh
+#  Universal USB Installer Copyright https://pendrivelinux.com
+#  Updates by Robert E. Novak aka REN
+#  sailnfool@gmail.com
+#  Cebu, Philippines
+#_____________________________________________________________________
+# Rev.|Aut| Date     | Notes
+#_____________________________________________________________________
+# 1.0 |REN|03/30/2024| Update by Robert E. Novak
+#                    | moved function mount_disk to rentoy_lib.sh
+#                    | REN version of ventoy_lib.sh
+#_____________________________________________________________________
+#
+
+ source ./tool/rentoy_lib.sh
+ source ./tool/ventoy_lib.sh
 
 print_usage() {
 
@@ -26,26 +40,31 @@ print_usage() {
     echo ''
 }
 
-mount_disk() {
-    DiskOrPart="$1"
-    # Define the mount point (where the disk/partition will be mounted)
-    MOUNT_POINT="/mnt/mounted_disk"  # You can choose a different mount point
-
-    # Check if the mount point directory exists, and if not, create it
-    if [ ! -d "$MOUNT_POINT" ]; then
-        mkdir -p "$MOUNT_POINT"
-    fi
-
-    # Mount the disk/partition to the specified mount point
-    mount "$DiskOrPart" "$MOUNT_POINT"
-    
-    # Check if the mount was successful
-    if [ $? -eq 0 ]; then
-        echo "Disk/Partition $DiskOrPart has been successfully mounted to $MOUNT_POINT."
-    else
-        echo "Failed to mount $DiskOrPart to $MOUNT_POINT."
-    fi
-}
+########################################################################
+# Shouldn't the following function be in "ventoy_lib.sh"?
+# Note that the only invocation of this function is commented out.
+# moved to ventoy_lib.sh
+########################################################################
+# mount_disk() {
+#     DiskOrPart="$1"
+#     # Define the mount point (where the disk/partition will be mounted)
+#     MOUNT_POINT="/mnt/mounted_disk"  # You can choose a different mount point
+# 
+#     # Check if the mount point directory exists, and if not, create it
+#     if [ ! -d "$MOUNT_POINT" ]; then
+#         mkdir -p "$MOUNT_POINT"
+#     fi
+# 
+#     # Mount the disk/partition to the specified mount point
+#     mount "$DiskOrPart" "$MOUNT_POINT"
+#     
+#     # Check if the mount was successful
+#     if [ $? -eq 0 ]; then
+#         echo "Disk/Partition $DiskOrPart has been successfully mounted to $MOUNT_POINT."
+#     else
+#         echo "Failed to mount $DiskOrPart to $MOUNT_POINT."
+#     fi
+# }
 
 SECUREBOOT="YES"
 VTNEW_LABEL='UUI'
