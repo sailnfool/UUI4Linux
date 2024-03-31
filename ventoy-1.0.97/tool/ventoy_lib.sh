96f6223f7cf17e54510a7b562e3631361b0559dc
Robert E. Novak <sailnfool@gmail.com>
2024-03-31 15:28:11 +0800

    Added ansi codes to rentoy which is sourced

--- ventoy-1.0.97/tool/ventoy_lib.sh
+++ ventoy-1.0.97/tool/ventoy_lib.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-
+#  Universal USB Installer Copyright https://pendrivelinux.com
+#  Updates by Robert E. Novak aka REN
+#  sailnfool@gmail.com
+#  Cebu, Philippines
+#_____________________________________________________________________
+# Rev.|Aut| Date     | Notes
+#_____________________________________________________________________
+# 1.0 |REN|03/30/2024| Update by Robert E. Novak
+#                    | Defined names of ansi terminal codes to change
+#                    | color of text and put them in rentoy_lib.sh
+#_____________________________________________________________________
+#
+VENT_VER="ventoy-1.0.97"
+source ${0%UUI4Linux*}/UUI4Linux/${VENT_VER}/tool/rentoy_lib.sh
 #Ventoy partition 32MB
 VENTOY_PART_SIZE=33554432
 VENTOY_PART_SIZE_MB=32
@@ -16,16 +29,19 @@ ventoy_true() {
 
 
 vtinfo() {
-    echo -e "\033[32m$*\033[0m"
+#    echo -e "\033[32m$*\033[0m"
+echo -e "${ansi_GREEN}$*${ansi_RESTORE}"
 }
 
 vtwarn() {
-    echo -e "\033[33m$*\033[0m"
+#    echo -e "\033[33m$*\033[0m"
+    echo -e "${ansi_YELLOW}$*${ansi_RESTORE}"
 }
 
 
 vterr() {
-    echo -e "\033[31m$*\033[0m"
+#    echo -e "\033[31m$*\033[0m"
+    echo -e "${ansi_RED}$*${ansi_RESTORE}"
 }
 
 vtdebug() {
@@ -105,6 +121,35 @@ check_umount_disk() {
         umount $mtpnt >/dev/null 2>&1
     done
 }
+
+########################################################################
+# Shouldn't the following function be in "ventoy_lib.sh"?
+# Note that the only invocation of this function in UUIWorker is
+# commented out. (REN)
+# moved this function to rentoy_lib.sh
+########################################################################
+# mount_disk() {
+#     DiskOrPart="$1"
+#     # Define the mount point (where the disk/partition will be mounted)
+#     MOUNT_POINT="/mnt/mounted_disk"  # You can choose a different mount point
+# 
+#     # Check if the mount point directory exists, and if not, create it
+#     if [ ! -d "$MOUNT_POINT" ]; then
+#         mkdir -p "$MOUNT_POINT"
+#     fi
+# 
+#     # Mount the disk/partition to the specified mount point
+#     mount "$DiskOrPart" "$MOUNT_POINT"
+#     
+#     # Check if the mount was successful
+#     if [ $? -eq 0 ]; then
+#         echo "Disk/Partition $DiskOrPart has been successfully mounted to $MOUNT_POINT."
+#     else
+#         echo "Failed to mount $DiskOrPart to $MOUNT_POINT."
+#     fi
+# }
+
+
 
 get_ventoy_version_from_cfg() {
     if grep -q 'set.*VENTOY_VERSION=' $1; then
